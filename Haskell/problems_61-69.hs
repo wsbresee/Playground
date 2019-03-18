679ebf98b9f45f0d8f1d7c42926bcb57afc32908
wsbresee <wsbresee@gmail.com>
2019-03-18 08:42:44 -0400

    solved a few more problems

--- Haskell/problems_61-69.hs
+++ Haskell/problems_61-69.hs
@@ -9,6 +9,10 @@ hbalTree e h = [Branch e left right | i     <- [h-1, h-2],
                                       left  <- hbalTree e i,
                                       right <- hbalTree e j]
 
+countNodes :: Tree a -> Int
+countNodes Empty = 0
+countNodes (Branch _ l r) = 1 + (countNodes l) + (countNodes r)
+
 -- Problem 61
 
 countLeaves :: Tree a -> Int
@@ -40,3 +44,48 @@ atLevel (Branch x l r) h
   | otherwise   = (atLevel l (h-1)) ++ (atLevel r (h-1))
 
 -- Problem 63
+
+completeBinaryTree :: [a] -> Tree a
+completeBinaryTree xs = cBT xs 0
+    where
+        cBT xs i
+          | i > ((length xs) - 1) = Empty
+          | otherwise       = (Branch (xs!!i) (cBT xs (2*i+1)) (cBT xs (2*i+2)))
+
+isCompleteBinaryTree :: Tree a -> Bool
+isCompleteBinaryTree t = treeEqual t $ completeBinaryTree $ take i [1..]
+    where
+        i = countNodes t
+        treeEqual Empty Empty = True
+        treeEqual Empty _     = False
+        treeEqual _     Empty = False
+        treeEqual (Branch _ l1 r1) (Branch _ l2 r2)
+          = (treeEqual l1 l2) && (treeEqual r1 r2)
+
+-- Problem 64
+
+layout :: Tree a -> Tree (a, (Int, Int))
+layout t = l' t 1 0
+    where
+        l' Empty _ _ = Empty
+        l' (Branch x l r) i j
+          = (Branch (x, (j', i)) (l' l (i+1) 0) (l' r (i+1) j'))
+              where
+                  j' = j + 1 + countNodes l
+
+-- Problem 65
+
+height :: Tree a -> Int
+height Empty = 0
+height (Branch _ l r) = maximum [height l, height r] + 1
+
+layout2 :: Tree a -> Tree (a, (Int, Int))
+layout2 t = lay' t (2^((height t)-1)) 1
+    where
+        lay' Empty x y = Empty
+        lay' (Branch elem l r) x y = (Branch (elem, (x, y)) l' r')
+            where
+                l' = lay' l (x-((2^((height l)))`div`2)) (y+1)
+                r' = lay' r (x+((2^((height r)))`div`2)) (y+1)
+
+-- Problem 66
